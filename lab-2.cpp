56b9f6c3231b99d5e4ec148ead8313035865d790
alexpaniman <alexpaniman@gmail.com>
2024-05-10 18:41:58 +0300

    feature: equal separation of tasks on threads

--- lab-2.cpp
+++ lab-2.cpp
@@ -1,10 +1,15 @@
+#include <chrono>
+#include <mutex>
 #include <numeric>
+#include <queue>
 #include <thread>
 #include <future>
 #include <cmath>
 #include <stdio.h>
 #include <vector>
 #include <iostream>
+#include <optional>
+#include <sstream>
 
 
 const size_t MAX_ITERATIONS = 32;
@@ -81,33 +86,104 @@ double integrate_to_precision(integration_scope scope, integration_method_func_t
     return NAN;
 }
 
+template <typename type>
+class locked_stack {
+public:
+    void push(type value) {
+        std::lock_guard<std::mutex> lock(mutex_);
+        stack_.push_back(std::move(value));
+    }
+
+    std::optional<type> pop() {
+        std::lock_guard<std::mutex> lock(mutex_);
+
+        if (stack_.empty())
+            return {};
+
+        type value = stack_.back();
+        stack_.pop_back();
+
+        return value;
+    }
+
+private:
+    std::vector<type> stack_;
+    std::mutex mutex_;
+};
+
+
+struct integration_task {
+    integration_scope scope;
+    int number_of_points;
+};
+
+class integrator_thread {
+public:
+    integrator_thread(double precison, locked_stack<integration_task> &tasks, double &result):
+        precision_(precison), tasks_(tasks), result_(result) {}
+
+
+    void operator()() {
+        while (std::optional<integration_task> maybe_task = tasks_.pop()) {
+            auto task = *maybe_task;
+
+            while (true) {
+                double previous_integral = integrate_trapezoid(task.scope, task.number_of_points);
+                task.number_of_points *= 2;
+
+                double current_integral = integrate_trapezoid(task.scope, task.number_of_points);
+                double difference = std::abs(previous_integral - current_integral);
+                if (difference < precision_) {
+                    result_ += current_integral;
+
+                    std::stringstream ss;
+                    ss << std::this_thread::get_id() << ":\t" << task.scope.a << "\t\t" << task.scope.b << "\n";
+
+                    std::cerr << ss.str();
+                    break;
+                }
+
+                // Didn't get good enough results yet...
+                integration_task new_task = task;
+                double middle_point = (task.scope.a + task.scope.b) / 2.;
+                new_task.scope.a = middle_point;
+
+                // Shrink current scope in half:
+                task.scope.b = middle_point;
+                tasks_.push(new_task); // Add half of the task into stack
+            }
+        }
+    }
+
+
+private:
+    double precision_;
+
+    locked_stack<integration_task> &tasks_;
+    double &result_;
+};
+
+
 
 
 int main() {
-    const std::size_t threads_num = 15;
-    const double precision = 1e-9;
+    const std::size_t threads_num = 32;
+    const double precision = 1e-05;
 
     integration_scope scope {
-        [](double x) { return sin(1/x); },
-        .00001, 1.
+        [](double x) { return sin(1./x); },
+        1e-5, 10000.
     };
 
-    std::vector<double> results(threads_num);
+    std::vector<double> results(threads_num, 0);
+
+    locked_stack<integration_task> tasks;
+    tasks.push({ scope, 2 });
+
     {
         std::vector<std::jthread> threads;
-
-        for (std::size_t i = 0; i < threads_num; ++i) {
-            double subscope_length = (scope.b - scope.a) / threads_num;
-            integration_scope subscope { scope.f,
-                scope.a + subscope_length * i,
-                scope.a + subscope_length * (i + 1)
-            };
-            std::cout << i << "\t[" << subscope.a << ",\t" << subscope.b << "]\n";
-
-            threads.emplace_back([=, &result = results[i]] {
-                result = integrate_to_precision(subscope, integrate_trapezoid, precision);
-            });
-        }
+        for (std::size_t i = 0; i < threads_num; ++i)
+            threads.emplace_back(integrator_thread(precision, tasks, results[i]));
     }
 
     double integral = std::accumulate(results.begin(), results.end(), 0.); 
